36352e466b71633b85251ed8ece6df8550802973
chandranshh <chandranshyay@gmail.com>
2023-06-24 22:49:27 +0530

    UI Changes - added a loader spinner while fetching data

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -2,17 +2,21 @@ import { useEffect, useState } from "react";
 import DetailsCard from "./modules/DetailsCard";
 import Selector from "./modules/Selector";
 import { fetchJobListings } from "../../controllers/fetchJobLisitngs";
+import LoadingSpinner from "./modules/LoadingSpinner/LoadingSpinner";
 
 function Sidebar() {
   const [jobData, setJobData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function fetchData() {
       try {
         const data = await fetchJobListings();
         setJobData(data);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching job listings:", error);
+        setLoading(false);
       }
     }
 
@@ -25,9 +29,16 @@ function Sidebar() {
         <Selector />
       </div>
       <div className="flex flex-col overflow-y-scroll scrollbar-hide h-[56%]">
-        {jobData.map((job) => (
-          <DetailsCard key={job.company} jobDetails={job} />
-        ))}
+        {loading ? (
+          <div className="flex justify-center items-center h-full">
+            <LoadingSpinner />
+            <div className="loader"></div>
+          </div>
+        ) : (
+          jobData.map((job) => (
+            <DetailsCard key={job.company} jobDetails={job} />
+          ))
+        )}
       </div>
     </div>
   );
---
+++ client/src/components/Sidebar/modules/LoadingSpinner/LoadingSpinner.jsx
@@ -0,0 +1,7 @@
+import "./LoadingSpinner.css"; // Import the CSS file for styling
+
+function LoadingSpinner() {
+  return <div className="loading-spinner"></div>;
+}
+
+export default LoadingSpinner;
