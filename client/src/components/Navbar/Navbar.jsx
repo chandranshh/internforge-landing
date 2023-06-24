649f0dc79d2755dc856cf6207282e30bbaea9891
chandranshh <chandranshyay@gmail.com>
2023-06-24 14:42:41 +0530

    selected tab will have a purple color

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import BellIcon from "../../assets/Navbar/notification.png";
 import UserImage from "../../assets/Navbar/user.png";
 import ClipBoard from "../../assets/Navbar/clipboardtext.png";
@@ -6,25 +7,49 @@ import TaskSquare from "../../assets/Navbar/tasksquare.png";
 import Edit from "../../assets/Navbar/edit.png";
 
 function Navbar() {
+  const [activeMenu, setActiveMenu] = useState("browse-listings");
+
+  const handleMenuClick = (menu) => {
+    setActiveMenu(menu);
+  };
+
+  const getMenuClass = (menu) => {
+    return activeMenu === menu
+      ? "text-[#6938EF] flex items-center gap-2 cursor-pointer"
+      : "text-[#667085] flex items-center gap-2 cursor-pointer";
+  };
+
   return (
     <div className="bg-[#FFFFFF] h-16 flex justify-around items-center shadow-md">
       <h1 className="font-bold text-black text-2xl cursor-pointer">
         InternBrand
       </h1>
       <div className="flex gap-8 font-bold">
-        <div className="text-[#6938EF] flex items-center gap-2 cursor-pointer">
+        <div
+          className={getMenuClass("browse-listings")}
+          onClick={() => handleMenuClick("browse-listings")}
+        >
           <img src={ClipBoard} alt="clipoard.png" />
           <div className="whitespace-nowrap">Browse Listings</div>
         </div>
-        <div className="text-[#667085] flex items-center gap-2 cursor-pointer">
+        <div
+          className={getMenuClass("application-history")}
+          onClick={() => handleMenuClick("application-history")}
+        >
           <img src={TaskSquare} alt="edit.png" />
           <div className="whitespace-nowrap">Application History</div>
         </div>
-        <div className="text-[#667085] flex items-center gap-2 cursor-pointer">
+        <div
+          className={getMenuClass("blog")}
+          onClick={() => handleMenuClick("blog")}
+        >
           <img src={Edit} alt="edit.png" />
           <div className="whitespace-nowrap">Blog</div>
         </div>
-        <div className="text-[#667085] flex items-center gap-2 cursor-pointer">
+        <div
+          className={getMenuClass("contact-us")}
+          onClick={() => handleMenuClick("contact-us")}
+        >
           <img src={Message} alt="messagequestion.png" />
           <div className="whitespace-nowrap">Contact Us</div>
         </div>
