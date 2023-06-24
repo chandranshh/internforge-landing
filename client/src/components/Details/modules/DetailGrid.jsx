2097fdc08fd890eeb96822f84e245413d12ba264
chandranshh <chandranshyay@gmail.com>
2023-06-24 22:43:05 +0530

    fixed remaining days date format issue

--- client/src/components/Details/modules/DetailGrid.jsx
+++ client/src/components/Details/modules/DetailGrid.jsx
@@ -7,7 +7,7 @@ import Location from "../../../assets/Details/location.png";
 import Clipboard from "../../../assets/Details/clipboardclose.png";
 import Ranking from "../../../assets/Details/ranking.png";
 import UserTick from "../../../assets/Details/usertick.png";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function DetailGrid() {
   const [remainingDays, setRemainingDays] = useState(0);
@@ -18,14 +18,14 @@ function DetailGrid() {
   const postedOn = date.toLocaleDateString();
 
   //endsBy
-  const endsByDate = new Date(selectedJob.endsBy);
-  const endsBy = endsByDate.toLocaleDateString();
+  useEffect(() => {
+    const endsByDate = new Date(selectedJob.endsBy);
+    const currentDate = new Date();
+    const differenceInTime = endsByDate.getTime() - currentDate.getTime();
+    const differenceInDays = Math.ceil(differenceInTime / (1000 * 3600 * 24));
 
-  //i want to calculate the raminmaining days from current date
-  const today = new Date();
-  const timeDiff = endsByDate.getTime() - today.getTime();
-  const daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
-  setRemainingDays(daysDiff);
+    setRemainingDays(differenceInDays);
+  }, [selectedJob.endsBy]);
 
   return (
     <div>
@@ -33,73 +33,71 @@ function DetailGrid() {
         <div className="flex items-center">
           <img className="h-[40px] w-[40px]" src={Timer} alt="timer.png" />
           <div className="flex flex-col justify-center items-start p-2">
-            <div className="font-bold text-[1rem] text-gray-700">
+            <div className="font-bold text-[1rem] text-black">
               {selectedJob.duration}
             </div>
-            <div className="text-[1rem] text-gray-700">Duration</div>
+            <div className="text-[1rem] text-black">Duration</div>
           </div>
         </div>
         <div className="flex items-center">
           <img className="h-[40px] w-[40px]" src={Ranking} alt="timer.png" />
           <div className="flex flex-col justify-center items-start p-2">
-            <div className="font-bold text-[1rem] text-gray-700">
+            <div className="font-bold text-[1rem] text-black">
               {selectedJob.experience}
             </div>
-            <div className="text-[1rem] text-gray-700">Experience</div>
+            <div className="text-[1rem] text-black">Experience</div>
           </div>
         </div>
         <div className="flex items-center">
           <img className="h-[40px] w-[40px]" src={Wallet} alt="timer.png" />
           <div className="flex flex-col justify-center items-start p-2">
-            <div className="font-bold text-[1rem] text-gray-700">
+            <div className="font-bold text-[1rem] text-black">
               {selectedJob.stipend}
             </div>
-            <div className="text-[1rem] text-gray-700">Stipend</div>
+            <div className="text-[1rem] text-black">Stipend</div>
           </div>
         </div>
         <div className="flex items-center">
           <img className="h-[40px] w-[40px]" src={Location} alt="timer.png" />
           <div className="flex flex-col justify-center items-start p-2">
-            <div className="font-bold text-[1rem] text-gray-700">
+            <div className="font-bold text-[1rem] text-black">
               {selectedJob.location}
             </div>
-            <div className="text-[1rem] text-gray-700">Location</div>
+            <div className="text-[1rem] text-black">Location</div>
           </div>
         </div>
         <div className="flex items-center">
           <img className="h-[40px] w-[40px]" src={Calendar} alt="timer.png" />
           <div className=" flex flex-col justify-center items-start p-2">
-            <div className="font-bold text-[1rem] text-gray-700">
-              {postedOn}
-            </div>
-            <div className="text-[1rem] text-gray-700">Posted On</div>
+            <div className="font-bold text-[1rem] text-black">{postedOn}</div>
+            <div className="text-[1rem] text-black">Posted On</div>
           </div>
         </div>
         <div className="flex items-center">
           <img className="h-[40px] w-[40px]" src={Clipboard} alt="timer.png" />
           <div className=" flex flex-col justify-center items-start p-2">
-            <div className="font-bold text-[1rem] text-gray-700">
-              {daysDiff}
+            <div className="font-bold text-[1rem] text-black">
+              {remainingDays} days
             </div>
-            <div className="text-[1rem] text-gray-700">Ends In</div>
+            <div className="text-[1rem] text-black">Ends In</div>
           </div>
         </div>
         <div className="flex items-center">
           <img className="h-[40px] w-[40px]" src={UserTick} alt="timer.png" />
           <div className="flex flex-col justify-center items-start  p-2">
-            <div className="font-bold text-[1rem] text-gray-700">
+            <div className="font-bold text-[1rem] text-black">
               {selectedJob.openPositions}
             </div>
-            <div className="text-[1rem] text-gray-700">Open Positions</div>
+            <div className="text-[1rem] text-black">Open Positions</div>
           </div>
         </div>
         <div className="flex items-center">
           <img className="h-[40px] w-[40px]" src={Applicants} alt="timer.png" />
           <div className="flex flex-col justify-center items-start p-2">
-            <div className="font-bold text-[1rem] text-gray-700">
+            <div className="font-bold text-[1rem] text-black">
               {selectedJob.applicants}
             </div>
-            <div className="text-[1rem] text-gray-700">Total Applicants</div>
+            <div className="text-[1rem] text-black">Total Applicants</div>
           </div>
         </div>
       </div>
