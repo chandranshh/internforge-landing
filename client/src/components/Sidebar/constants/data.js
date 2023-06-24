343b02127c9dc80ff0e2c229866b7a8e078f61c3
chandranshh <chandranshyay@gmail.com>
2023-06-24 14:42:53 +0530

    some data changes

--- client/src/components/Sidebar/constants/data.js
+++ client/src/components/Sidebar/constants/data.js
@@ -7,112 +7,322 @@ export const jobData = [
     duration: "3 months",
     stipend: "Rs. 10,000",
     applicants: "123",
+    postedOn: "2023-06-27",
     endsBy: "2023-07-27",
+    location: "Noida",
+    openPositions: "10",
+    experience: "Intermediate",
+    aboutUs:
+      "Xapo Visuals is a creative studio that specializes in motion graphics and animation. We are a team of 10 people and have worked with over 100 clients in the past 5 years. We are looking for a motion graphics designer to join our team.",
+    requirements: `- Proficiency in Adobe After Effects, Photoshop, and Illustrator
+    - Experience in Blender and Figma is a plus
+    - Ability to work in a team
+    - Good communication skills
+    - Ability to work under pressure and meet deadlines`,
+    responsibilities: `- Create motion graphics and animations for our clients
+    - Work with the team to create storyboards and scripts
+    - Work on multiple projects at the same time
+    - Meet deadlines and deliver high-quality work`,
   },
   {
-    jobTitle: "Frontend Developer",
-    company: "TechCo Solutions",
-    companyLogo: "https://i.imgur.com/5jOJqU9.png",
-    skills: ["HTML", "CSS", "JavaScript", "React", "Redux"],
-    duration: "6 months",
-    stipend: "Rs. 15,000",
-    applicants: "87",
-    endsBy: "2023-08-15",
-  },
-  {
-    jobTitle: "Marketing Intern",
-    company: "Digital Marketing Agency",
-    companyLogo: "https://i.imgur.com/2uKYsMx.png",
-    skills: ["SEO", "Google Analytics"],
-    duration: "2 months",
-    stipend: "Rs. 8,000",
-    applicants: "45",
-    endsBy: "2023-09-10",
-  },
-  {
-    jobTitle: "Data Analyst",
-    company: "Analytics Inc.",
-    companyLogo: "https://i.imgur.com/8L37U3r.png",
-    skills: ["SQL", "Python", "Data Visualization", "Statistical Analysis"],
-    duration: "4 months",
-    stipend: "Rs. 12,000",
-    applicants: "65",
-    endsBy: "2023-10-05",
-  },
-  {
-    jobTitle: "UI/UX Designer",
-    company: "Creative Studio",
-    companyLogo: "https://i.imgur.com/jFjoxHc.png",
+    jobTitle: "Digital Marketing Specialist",
+    company: "DigitalBoost Agency",
+    companyLogo: "https://i.imgur.com/IJKL789.png",
     skills: [
-      "User Research",
-      "Wireframing",
-      "Prototyping",
-      "Usability Testing",
+      "SEO",
+      "SEM",
+      "Social Media Marketing",
+      "Google Analytics",
+      "Copywriting",
     ],
-    duration: "3 months",
-    stipend: "Rs. 9,000",
-    applicants: "54",
-    endsBy: "2023-11-21",
-  },
-  {
-    jobTitle: "Backend Developer",
-    company: "Tech Innovators",
-    companyLogo: "https://i.imgur.com/Qf3SQiw.png",
-    skills: ["Java", "Spring Boot", "MySQL", "RESTful APIs"],
-    duration: "6 months",
+    duration: "9 months",
     stipend: "Rs. 14,000",
-    applicants: "78",
-    endsBy: "2023-12-15",
-  },
-  // Additional data entries
-  {
-    jobTitle: "Graphic Designer",
-    company: "Creative Agency",
-    companyLogo: "https://i.imgur.com/A3XFAvG.png",
-    skills: ["Illustrator", "InDesign", "Typography"],
-    duration: "2 months",
-    stipend: "Rs. 8,500",
-    applicants: "32",
-    endsBy: "2023-12-30",
-  },
-  {
-    jobTitle: "Full Stack Developer",
-    company: "Tech Solutions",
-    companyLogo: "https://i.imgur.com/4IQbhT7.png",
-    skills: ["JavaScript", "Node.js", "React", "MongoDB"],
-    duration: "8 months",
-    stipend: "Rs. 18,000",
-    applicants: "96",
-    endsBy: "2024-01-20",
-  },
-  {
-    jobTitle: "Content Writer",
-    company: "Digital Agency",
-    companyLogo: "https://i.imgur.com/jZkQ0LW.png",
-    skills: ["Copywriting", "SEO", "Blog Writing"],
-    duration: "3 months",
-    stipend: "Rs. 9,500",
-    applicants: "42",
-    endsBy: "2024-02-05",
+    applicants: "75",
+    postedOn: "2023-07-03",
+    endsBy: "2024-04-03",
+    location: "Chennai",
+    openPositions: "4",
+    experience: "Mid-level",
+    aboutUs:
+      "DigitalBoost Agency is a leading digital marketing agency focused on helping businesses grow their online presence. We are seeking a talented digital marketing specialist to join our team and implement effective strategies to drive organic and paid traffic.",
+    requirements: `- Strong knowledge of SEO and SEM principles
+  - Experience with social media marketing and advertising
+  - Proficiency in Google Analytics and other marketing tools
+  - Excellent copywriting and content creation skills
+  - Analytical mindset and ability to analyze campaign performance`,
+    responsibilities: `- Develop and implement digital marketing campaigns
+  - Optimize website content and landing pages for search engines
+  - Manage social media accounts and run paid advertising campaigns
+  - Monitor and analyze campaign performance metrics`,
   },
   {
     jobTitle: "Software Engineer",
-    company: "Tech Solutions Ltd.",
-    companyLogo: "https://i.imgur.com/YLrg2jc.png",
-    skills: ["Java", "Spring", "Hibernate", "MySQL"],
+    company: "Infinite Loop Technologies",
+    companyLogo: "https://i.imgur.com/MNOP123.png",
+    skills: ["Java", "Python", "C++"],
     duration: "12 months",
-    stipend: "Rs. 20,000",
-    applicants: "115",
-    endsBy: "2024-03-15",
+    stipend: "Rs. 25,000",
+    applicants: "112",
+    postedOn: "2023-07-04",
+    endsBy: "2024-07-04",
+    location: "Hyderabad",
+    openPositions: "6",
+    experience: "Senior",
+    aboutUs:
+      "Infinite Loop Technologies is a software development company specializing in enterprise solutions. Join our team of talented engineers to build scalable and robust software products using cutting-edge technologies.",
+    requirements: `- Strong programming skills in Java, Python, and C++
+  - Experience with database systems and SQL
+  - Knowledge of agile development methodologies
+  - Familiarity with software testing and debugging
+  - Strong problem-solving and teamwork skills`,
+    responsibilities: `- Develop high-quality software applications
+  - Collaborate with cross-functional teams on project requirements
+  - Write clean, efficient, and maintainable code
+  - Perform unit testing and debugging`,
   },
   {
-    jobTitle: "Social Media Manager",
-    company: "Marketing Agency",
-    companyLogo: "https://i.imgur.com/dM9SfC2.png",
-    skills: ["Content Creation", "Analytics"],
+    jobTitle: "Web Developer",
+    company: "CodeCraft Solutions",
+    companyLogo: "https://i.imgur.com/ABCD123.png",
+    skills: ["HTML", "CSS", "JavaScript", "React", "Node.js"],
+    duration: "12 months",
+    stipend: "Rs. 20,000",
+    applicants: "142",
+    postedOn: "2023-07-01",
+    endsBy: "2024-07-01",
+    location: "Delhi",
+    openPositions: "3",
+    experience: "Senior",
+    aboutUs:
+      "CodeCraft Solutions is a software development company specializing in web and mobile applications. We are seeking experienced web developers to join our team and work on exciting projects using the latest technologies.",
+    requirements: `- Strong proficiency in HTML, CSS, and JavaScript
+    - Experience with React and Node.js
+    - Knowledge of responsive web design
+    - Familiarity with version control systems (e.g., Git)
+    - Strong problem-solving and debugging skills`,
+    responsibilities: `- Develop and maintain web applications
+    - Collaborate with designers and backend developers
+    - Implement responsive and accessible web designs
+    - Write clean and efficient code`,
+  },
+  {
+    jobTitle: "Data Analyst",
+    company: "Insights Analytics",
+    companyLogo: "https://i.imgur.com/EFGH456.png",
+    skills: [
+      "SQL",
+      "Python",
+      "Data Visualization",
+      "Statistical Analysis",
+      "Excel",
+    ],
+    duration: "6 months",
+    stipend: "Rs. 18,000",
+    applicants: "98",
+    postedOn: "2023-07-02",
+    endsBy: "2023-12-02",
+    location: "Pune",
+    openPositions: "2",
+    experience: "Intermediate",
+    aboutUs:
+      "Insights Analytics is a data-driven consulting firm helping businesses make informed decisions. We are looking for skilled data analysts who can extract insights from complex data sets and present them in a meaningful way to our clients.",
+    requirements: `- Proficiency in SQL and Python
+    - Experience with data visualization tools (e.g., Tableau, Power BI)
+    - Strong analytical and problem-solving skills
+    - Knowledge of statistical analysis techniques
+    - Advanced Excel skills`,
+    responsibilities: `- Collect, clean, and analyze large datasets
+    - Create interactive visualizations and reports
+    - Perform statistical analysis and generate insights
+    - Present findings to clients in a clear and concise manner`,
+  },
+  {
+    jobTitle: "Graphic Designer",
+    company: "Pixel Perfect Design",
+    companyLogo: "https://i.imgur.com/3H24G0s.png",
+    skills: ["Photoshop", "Illustrator", "InDesign", "Sketch", "Typography"],
+    duration: "6 months",
+    stipend: "Rs. 15,000",
+    applicants: "87",
+    postedOn: "2023-06-28",
+    endsBy: "2023-12-28",
+    location: "Mumbai",
+    openPositions: "5",
+    experience: "Entry level",
+    aboutUs:
+      "Pixel Perfect Design is a design agency specializing in graphic design and branding solutions. With a team of 15 designers, we have served clients across various industries. Join us to create visually stunning designs that leave a lasting impact.",
+    requirements: `- Proficiency in Adobe Photoshop, Illustrator, and InDesign
+    - Experience with Sketch and knowledge of typography
+    - Strong creativity and attention to detail
+    - Ability to collaborate effectively with team members
+    - Excellent time management skills`,
+    responsibilities: `- Design graphics for print and digital media
+    - Create brand identities and marketing collateral
+    - Collaborate with clients to understand their design needs
+    - Ensure timely delivery of design projects`,
+  },
+  {
+    jobTitle: "UX/UI Designer",
+    company: "Digital Innovators",
+    companyLogo: "https://i.imgur.com/Awtk0CD.png",
+    skills: ["Wireframing", "Prototyping", "Figma"],
     duration: "4 months",
-    stipend: "Rs. 11,000",
-    applicants: "58",
-    endsBy: "2024-04-10",
+    stipend: "Rs. 12,000",
+    applicants: "64",
+    postedOn: "2023-06-29",
+    endsBy: "2023-10-29",
+    location: "Bangalore",
+    openPositions: "8",
+    experience: "Junior",
+    aboutUs:
+      "Digital Innovators is a technology company focused on creating innovative digital solutions. We are seeking a talented UX/UI designer to join our team and help us craft intuitive and visually appealing user experiences for our products.",
+    requirements: `- Solid understanding of user-centered design principles
+    - Proficiency in wireframing and prototyping tools
+    - Experience with usability testing and user research
+    - Proficiency in Adobe XD and Figma
+    - Strong problem-solving skills`,
+    responsibilities: `- Design user interfaces for web and mobile applications
+    - Create wireframes, prototypes, and user flows
+    - Conduct usability testing and iterate based on feedback
+    - Collaborate with development teams to implement designs`,
+  },
+  {
+    jobTitle: "Content Writer",
+    company: "WordSmith Creative",
+    companyLogo: "https://i.imgur.com/QWERT789.png",
+    skills: ["Copywriting", "SEO"],
+    duration: "6 months",
+    stipend: "Rs. 12,000",
+    applicants: "50",
+    postedOn: "2023-07-05",
+    endsBy: "2023-12-05",
+    location: "Kolkata",
+    openPositions: "3",
+    experience: "Entry level",
+    aboutUs:
+      "WordSmith Creative is a content marketing agency dedicated to crafting compelling and engaging content for businesses. We are looking for talented content writers who can create captivating copy that resonates with our clients' target audiences.",
+    requirements: `- Excellent writing and editing skills
+    - Knowledge of SEO best practices
+    - Strong research and analytical abilities
+    - Attention to detail and grammar proficiency
+    - Ability to meet deadlines`,
+    responsibilities: `- Write engaging and persuasive copy for various platforms
+    - Conduct research to generate unique content ideas
+    - Optimize content for search engine visibility
+    - Proofread and edit content for grammar and clarity`,
+  },
+  {
+    jobTitle: "Front-end Developer",
+    company: "PixelPerfect Web Solutions",
+    companyLogo: "https://i.imgur.com/ASDFG456.png",
+    skills: ["HTML", "CSS", "JavaScript", "React"],
+    duration: "9 months",
+    stipend: "Rs. 18,000",
+    applicants: "92",
+    postedOn: "2023-07-06",
+    endsBy: "2024-04-06",
+    location: "Jaipur",
+    openPositions: "2",
+    experience: "Mid-level",
+    aboutUs:
+      "PixelPerfect Web Solutions is a web development agency specializing in creating visually stunning and user-friendly websites. Join our team of skilled developers to bring design concepts to life and deliver exceptional user experiences.",
+    requirements: `- Strong knowledge of HTML, CSS, and JavaScript
+    - Experience with front-end frameworks like React
+    - Familiarity with responsive design principles
+    - Proficiency in cross-browser compatibility
+    - Attention to detail and pixel-perfect implementation`,
+    responsibilities: `- Develop and implement front-end solutions
+    - Collaborate with designers to translate mockups into code
+    - Ensure cross-browser and device compatibility
+    - Optimize websites for performance and usability`,
+  },
+  {
+    jobTitle: "Business Analyst",
+    company: "Strategic Insights Consulting",
+    companyLogo: "https://i.imgur.com/ZXCVB789.png",
+    skills: [
+      "Business Analysis",
+      "Data Analysis",
+      "Requirements Gathering",
+      "Project Management",
+      "Presentation",
+    ],
+    duration: "12 months",
+    stipend: "Rs. 20,000",
+    applicants: "78",
+    postedOn: "2023-07-07",
+    endsBy: "2024-07-07",
+    location: "Gurgaon",
+    openPositions: "4",
+    experience: "Senior",
+    aboutUs:
+      "Strategic Insights Consulting is a management consulting firm specializing in strategic planning and business optimization. We are seeking experienced business analysts to help our clients identify opportunities and make data-driven decisions.",
+    requirements: `- Strong business analysis skills and experience
+    - Proficiency in data analysis and visualization
+    - Ability to gather and document requirements
+    - Project management and stakeholder coordination
+    - Excellent communication and presentation skills`,
+    responsibilities: `- Conduct business analysis and gather requirements
+    - Analyze data and provide actionable insights
+    - Assist in project planning and coordination
+    - Prepare and deliver client presentations`,
+  },
+  {
+    jobTitle: "UI/UX Designer",
+    company: "DesignLab Solutions",
+    companyLogo: "https://i.imgur.com/QWERT123.png",
+    skills: ["User Research", "Wireframing", "Prototyping", "Adobe XD"],
+    duration: "6 months",
+    stipend: "Rs. 15,000",
+    applicants: "68",
+    postedOn: "2023-07-08",
+    endsBy: "2023-12-08",
+    location: "Ahmedabad",
+    openPositions: "2",
+    experience: "Intermediate",
+    aboutUs:
+      "DesignLab Solutions is a design agency focused on creating intuitive and visually appealing user interfaces. Join our team of talented designers to create user-centered designs that enhance the overall user experience.",
+    requirements: `- Experience in user research and creating user personas
+    - Proficiency in wireframing and prototyping tools
+    - Knowledge of usability testing methodologies
+    - Strong understanding of design principles and typography
+    - Proficiency in Adobe XD or similar design tools`,
+    responsibilities: `- Conduct user research and create user personas
+    - Design wireframes and interactive prototypes
+    - Conduct usability testing and iterate based on feedback
+    - Collaborate with developers to implement designs`,
+  },
+  {
+    jobTitle: "Full Stack Developer",
+    company: "TechSolutions Ltd.",
+    companyLogo: "https://i.imgur.com/ASDFG123.png",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Node.js",
+      "MongoDB",
+      "React",
+      "Next.Js",
+    ],
+    duration: "12 months",
+    stipend: "Rs. 22,000",
+    applicants: "105",
+    postedOn: "2023-07-09",
+    endsBy: "2024-07-09",
+    location: "Coimbatore",
+    openPositions: "3",
+    experience: "Senior",
+    aboutUs:
+      "TechSolutions Ltd. is a technology consulting company providing end-to-end software solutions. We are seeking skilled full stack developers to join our team and contribute to the development of complex web applications using modern technologies.",
+    requirements: `- Strong knowledge of HTML, CSS, and JavaScript
+    - Experience with Node.js and backend development
+    - Familiarity with front-end frameworks like React
+    - Proficiency in database technologies such as MongoDB
+    - Strong problem-solving and debugging skills`,
+    responsibilities: `- Develop and maintain web applications
+    - Implement both front-end and back-end functionalities
+    - Collaborate with cross-functional teams on project requirements
+    - Write clean and efficient code`,
   },
 ];
