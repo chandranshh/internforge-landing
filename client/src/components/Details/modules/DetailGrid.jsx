6ac098125c724713a41912eaed603ce7e21388e1
chandranshh <chandranshyay@gmail.com>
2023-06-24 22:36:32 +0530

    date format issue

--- client/src/components/Details/modules/DetailGrid.jsx
+++ client/src/components/Details/modules/DetailGrid.jsx
@@ -7,8 +7,10 @@ import Location from "../../../assets/Details/location.png";
 import Clipboard from "../../../assets/Details/clipboardclose.png";
 import Ranking from "../../../assets/Details/ranking.png";
 import UserTick from "../../../assets/Details/usertick.png";
+import { useState } from "react";
 
 function DetailGrid() {
+  const [remainingDays, setRemainingDays] = useState(0);
   const selectedJob = useSelector((state) => state.selectedJob.selectedJob);
 
   //postedOn
@@ -18,6 +20,12 @@ function DetailGrid() {
   //endsBy
   const endsByDate = new Date(selectedJob.endsBy);
   const endsBy = endsByDate.toLocaleDateString();
+
+  //i want to calculate the raminmaining days from current date
+  const today = new Date();
+  const timeDiff = endsByDate.getTime() - today.getTime();
+  const daysDiff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+  setRemainingDays(daysDiff);
 
   return (
     <div>
@@ -70,7 +78,9 @@ function DetailGrid() {
         <div className="flex items-center">
           <img className="h-[40px] w-[40px]" src={Clipboard} alt="timer.png" />
           <div className=" flex flex-col justify-center items-start p-2">
-            <div className="font-bold text-[1rem] text-gray-700">{endsBy}</div>
+            <div className="font-bold text-[1rem] text-gray-700">
+              {daysDiff}
+            </div>
             <div className="text-[1rem] text-gray-700">Ends In</div>
           </div>
         </div>
